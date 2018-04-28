b409fedc00e17f5aa3b814f86a46431bd5cc7695
Neo <neosh11@gmail.com>
2018-04-28 23:44:37 +0800

    Made it work on MacOs

--- testServer/pyex.py
+++ testServer/pyex.py
@@ -1,65 +1,74 @@
 #!/usr/bin/env python
  
 import string,cgi,time
-from os import curdir
+from os import curdir, path
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
  
+CURRENT_DIR = path.dirname(__file__)
 
-# Multitreaded server declaration
+# Multithreaded server declaration
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
   pass
-
 
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
  
   # GET
   def do_GET(self):
-        # Send response status code
-        self.send_response(200)
 
         # If public what it look like
-        x = curdir+"/public"+self.path
+        x = CURRENT_DIR+"/public"+self.path
+        print(self.path)
+
+        # Security for the filesystem
+        # If x is not contained within public folder
+        # Get absolute path of x and compare to absolute path of public directory
 
         # Controllers
         if(self.path == "/home"):
+          self.send_response(200)
           # Send headers
           self.send_header('Content-type', "text/html")
           self.end_headers()
-          f = open(curdir + "/home.html", 'rb')
+          f = open(CURRENT_DIR + "/home.html", 'rb')
           self.wfile.write(f.read())
           f.close()
         elif(self.path == "/login"):
+          self.send_response(200)
           # Send headers
           self.send_header('Content-type', "text/html")
           self.end_headers()
-          f = open(curdir + "/login.html", 'rb')
+          f = open(CURRENT_DIR + "/login.html", 'rb')
           self.wfile.write(f.read())
+          
           f.close()
         elif(self.path == "/haha"):
+          self.send_response(200)
           # Send headers
           self.send_header('Content-type', "text/html")
           self.end_headers()
           self.wfile.write(bytes("HAHA YOU SUCK :)", "utf-8"))
 
-        elif(Path(x).is_file() ):
+        elif(Path(x).is_file()):
+          self.send_response(200)
           #Check public folder for resources.
           self.send_header('Content-type', "text/html")
           self.end_headers()
           self.wfile.write(bytes("IN PUBLIC FOLDER MANN", "utf-8"))
         else:
+          self.send_response(404)
           self.send_header('Content-type', "text/html")
           self.end_headers()
-          self.wfile.write(bytes("HAHA YOU SUCK :)", "utf-8"))
+          self.wfile.write(bytes("Not found :(", "utf-8"))
 
         return
 
 def run():
   print('starting server...')
   # Server settings
-  server_address = ('127.0.0.1', 80)
+  server_address = ('0.0.0.0', 3000)
   httpd = ThreadingHTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
