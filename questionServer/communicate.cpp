fd8ce6884252db49e7eebf784dd3ceb4e6ceada2
neosh <neosh11@gmail.com>
2018-05-01 18:42:11 +0800

    Fixed qServer and tServer, now works nicely

--- questionServer/communicate.cpp
+++ questionServer/communicate.cpp
@@ -1,12 +1,19 @@
 #include "server.h"
 
+using namespace std;
+
+
+void printStr(char * str, int n){
+    for(int i = 0; i < n; i++){
+        cout << str[i];
+    }
+}
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-using namespace std;
 void action(int sock)
 {
     int n;
@@ -15,6 +22,7 @@ void action(int sock)
     //TODO
     // Needs an upgrade, need to get bytes rather than chars.
     std::cout << "Here is the message: ";
+
     while (1)
     {
         bzero(buffer, BUFFER_STD);
@@ -39,33 +47,42 @@ void action(int sock)
 void action2(int sock)
 {
     int n;
-    int *num;
-    // Write client's message to buffer
-    //TODO
-    // Needs an upgrade, need to get bytes rather than chars.
-    std::cout << "Here is the message: ";
+    int size_buff;
+    int type_buff;
 
-    bzero((void*)num, 4);
-    // Write client's message to buffer
-    //TODO
-    // Needs an upgrade, need to get bytes rather than chars.
-    if ((n = read(sock, (void *)num, 4)) < 0)
+    /********GET SIZE FROM HEADER********/
+    if ((n = read(sock, &size_buff, 4)) < 0)
         error("ERROR reading from socket");
+    cout << size_buff << "\n";
 
-    cout << *num << "\n";    
-    //Write to client
-    if ((n = write(sock, (void*)num, n)) < 0)
+    if(n == 0){
+        return;
+    }
+    // if(n==0){}
+    if ((n = write(sock, &size_buff, n)) < 0)
+        error("ERROR writing to socket");
+    
+    if(n == 0){
+        return;
+    }
+    /********GET TYPE FROM HEADER********/
+    if ((n = read(sock, &type_buff, 4)) < 0)
+        error("ERROR reading from socket");
+    cout << type_buff << "\n";
+    // if(n==0){}
+    if ((n = write(sock, &type_buff, n)) < 0)
         error("ERROR writing to socket");
 
-    char type_buff[5];
-    bzero(type_buff, 5);
-    if ((n = read(sock, type_buff, 5)) < 0)
-        error("ERROR reading from socket");
-    
-    cout << type_buff << "\n";  
-    
-    if ((n = write(sock, (void*)num, n)) < 0)
-        error("ERROR writing to socket");
-
+    char content_buff[BUFFER_STD];
+    for (int i = 0; i < size_buff; i++)
+    {
+        bzero(content_buff, BUFFER_STD);
+        if ((n = read(sock, content_buff, BUFFER_STD)) < 0)
+            error("ERROR reading from socket"); //remove
+        printStr(content_buff, n);
+        // if(n==0){}
+        if ((n = write(sock, content_buff, n)) < 0)
+            error("ERROR writing to socket"); //remove
+    }
     cout << endl;
 }
--- testServer/server_comms.py
+++ testServer/server_comms.py
@@ -1,7 +1,33 @@
 import socket
 import sys
+import math
 
 # Create a TCP/IP socket
+BUFFER_STD = 4
+
+def packData(content, type):
+    size = math.ceil((len(bytes(inp, 'utf-8')))/BUFFER_STD)
+    return ((size).to_bytes(4, "little"),(type).to_bytes(4, "little"),bytes(content, 'utf-8'))
+
+def sendData(sock, data):
+    sock.sendall(data[0])
+    sock.sendall(data[1])
+    sock.sendall(data[2])
+    
+def receiveData(sock):
+    size_data = int.from_bytes(sock.recv(4), byteorder='little')
+    type_data = int.from_bytes(sock.recv(4), byteorder='little')
+    # content_data = sock.recv(BUFFER_STD*size_data)
+    first = True
+    for i in range (0,size_data):
+        if(first):
+            first = False
+            content_data = sock.recv(BUFFER_STD)
+        else:
+            content_data += sock.recv(BUFFER_STD)
+
+    
+    return (size_data, type_data, content_data.decode())
 
 
 # Connect the socket to the port where the server is listening
@@ -9,38 +35,21 @@ server_address = ('localhost', 5000)
 print('connecting to %s port %s' % server_address)
 inp = ""
 try:
-    # while(inp !="quit"):
+    while(True):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(server_address)
+        # Get input
+        inp = input("message: ")
+        # Quit if input is quit
+        if(inp == "quit"):
+            break
+        message = packData(inp, 3)
+
         # Send data
-        # inp = input("message: ")
-        # message = inp
-        # print('sending "%s"' % message)
-
-        size = 4
-
-        message =(421452).to_bytes(4, "little")
-        message += bytes("hell",'utf-8')
-
-        sock.sendall(message)
-        print(len(message))
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        # print(bytes(message, 'utf-8'))
-        first = True
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            if(first):
-                first= False
-                totdata = data
-            else:
-                totdata+=data
-            amount_received += len(data)
-        print('received: ')
-        print(totdata)
-        print('closing socket')
-        sock.close()
+        print("sending")
+        sendData(sock, message)
+        # Receive response!
+        print(receiveData(sock))
 finally:
     print("fin")
     
